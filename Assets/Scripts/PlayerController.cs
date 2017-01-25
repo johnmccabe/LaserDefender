3a4b19e4f36cf578c0ca73e90c406939ef129606
John McCabe <john@johnmccabe.net>
2017-01-25 23:52:09 +0000

    add ship and playercontroller
    note use of `speed * Time.deltaTime` to keep movement independent of
    framerate

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 15f;
+	
+	// Update is called once per frame
+	void Update () {
+		MoveWithKeyboard ();
+	}
+
+	void MoveWithKeyboard() {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position += new Vector3 (-speed * Time.deltaTime, 0, 0);
+		}
+		else if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.position += new Vector3 (speed* Time.deltaTime, 0, 0);
+		}
+	}
+}
