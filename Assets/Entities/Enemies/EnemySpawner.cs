542a5a194384937f16c2bccebfc19eff85a9d5eb
John McCabe <john@johnmccabe.net>
2017-01-26 00:30:57 +0000

    create enemy and spawner, also reorg dirs

---
+++ Assets/Entities/Enemies/EnemySpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+		// need as GameObject as Instantiate returns an Object
+		GameObject enemy = Instantiate (enemyPrefab, new Vector3 (0, 0, 0), Quaternion.identity) as GameObject;
+		enemy.transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
