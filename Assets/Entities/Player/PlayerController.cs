294f1a0c233954b98b0428c70f53959d3bd55c3f
John McCabe <john@johnmccabe.net>
2017-01-30 00:01:10 +0000

    spawning projectiles

--- Assets/Entities/Player/PlayerController.cs
+++ Assets/Entities/Player/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed = 15f;
 	public float padding = 0.5f;
+	public GameObject projectile;
+	public float projectileSpeed;
+	public float firingRate = 0.2f;
 
 	float xmin, xmax;
 
@@ -22,7 +25,18 @@ public class PlayerController : MonoBehaviour {
 		MoveWithKeyboard ();
 	}
 
+	void Fire() {
+		GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+		beam.GetComponent<Rigidbody2D>().velocity = new Vector3 (0, projectileSpeed, 0);
+	}
+
 	void MoveWithKeyboard() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			InvokeRepeating ("Fire", 0.000001f, firingRate);
+		}
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			CancelInvoke ("Fire");
+		}
 		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) {
 			transform.position += Vector3.left * speed * Time.deltaTime;
 		}
