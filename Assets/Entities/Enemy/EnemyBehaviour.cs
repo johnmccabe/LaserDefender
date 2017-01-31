1056a2f26937f1fb7e10ba57cf4f084310d366e4
John McCabe <john@johnmccabe.net>
2017-01-31 21:41:52 +0000

    handle projectile/enemy interaction

---
+++ Assets/Entities/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	public float health = 150f;
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		Projectile missile = collider.gameObject.GetComponent<Projectile> ();
+		if (missile) {
+			health -= missile.GetDamage ();
+			missile.Hit ();
+			if (health <= 0) {
+				Destroy (gameObject);
+			}
+			Debug.Log ("Hit by a projectile");
+		}
+	}
+
+}
